2edee17b1610a055916045840194be0556ee70ad
BrowserAdventures <garretbonkowski@protonmail.com>
2018-06-08 17:57:13 -0700

    greet new user and alert others

--- server/server.js
+++ server/server.js
@@ -17,11 +17,30 @@ io.on('connection', (socket)=> {
 
     socket.on('createMessage', (message)=> {
         console.log('createMessage', message)
-        io.emit('newMessage', {
-            from: message.from,
-            text: message.text,
+
+        socket.emit('newMessage', {
+            from: 'Admin',
+            text: 'Welcome to chat app',
             createdAt: new Date().getTime()
         })
+
+        socket.broadcast.emit('newMessage', {
+            from: 'Admin',
+            text: 'New user joined',
+            createdAt: new Date().getTime()
+        })
+
+        // io.emit('newMessage', {
+        //     from: message.from,
+        //     text: message.text,
+        //     createdAt: new Date().getTime()
+        // })
+        // socket.broadcast.emit('newMessage', {
+        //     from: message.from,
+        //     text: message.text,
+        //     createdAt: new Date().getTime()
+        // })
+
     })
 
     socket.on('disconnect', ()=>  {
